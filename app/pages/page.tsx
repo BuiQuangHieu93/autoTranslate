7f7a6ac92dc882cb358931db3ee85df64ab750ad
BuiQuangHieu93 <zzzqhieu@gmail.com>
2024-06-01 23:45:15 +0700

    update ui

--- app/pages/page.tsx
+++ app/pages/page.tsx
@@ -2,34 +2,40 @@
 import { useState, ChangeEvent } from "react";
 
 export default function Home() {
-  const [selectedImage, setSelectedImage] = useState<File | null>(null);
-  const [translatedImage, setTranslatedImage] = useState<string | null>(null);
-  const [targetLang, setTargetLang] = useState<string>("es"); // Default target language is Spanish
+  const [selectedImages, setSelectedImages] = useState<File[]>([]);
+  const [translatedImages, setTranslatedImages] = useState<string[]>([]);
+  const [targetLang, setTargetLang] = useState<string>("vi"); // Default target language is Spanish
 
   const handleImageUpload = async (event: ChangeEvent<HTMLInputElement>) => {
-    const file = event.target.files?.[0];
-    if (file) {
-      setSelectedImage(file);
+    const files = event.target.files;
+    if (files) {
+      const fileArray = Array.from(files);
+      setSelectedImages(fileArray);
 
-      const formData = new FormData();
-      formData.append("file", file);
-      formData.append("target_lang", targetLang);
+      const translatedImagePromises = fileArray.map(async (file) => {
+        const formData = new FormData();
+        formData.append("file", file);
+        formData.append("target_lang", targetLang);
 
-      const response = await fetch(
-        "http://localhost:5000/translate_and_replace",
-        {
-          method: "POST",
-          body: formData,
-        }
-      );
+        const response = await fetch(
+          "http://localhost:5000/translate_and_replace",
+          {
+            method: "POST",
+            body: formData,
+          }
+        );
 
-      const data = await response.json();
-      const hexImage = data.image;
-      const base64Image = `data:image/png;base64,${Buffer.from(
-        hexImage,
-        "hex"
-      ).toString("base64")}`;
-      setTranslatedImage(base64Image);
+        const data = await response.json();
+        const hexImage = data.image;
+        const base64Image = `data:image/png;base64,${Buffer.from(
+          hexImage,
+          "hex"
+        ).toString("base64")}`;
+        return base64Image;
+      });
+
+      const translatedImageArray = await Promise.all(translatedImagePromises);
+      setTranslatedImages(translatedImageArray);
     }
   };
 
@@ -42,8 +48,9 @@ export default function Home() {
           id="targetLang"
           value={targetLang}
           onChange={(e) => setTargetLang(e.target.value)}
+          className="text-black"
         >
-          <option value="vi">Vietnam</option>
+          <option value="vi">Vietnamese</option>
           <option value="es">Spanish</option>
           <option value="fr">French</option>
           <option value="de">German</option>
@@ -51,21 +58,34 @@ export default function Home() {
           {/* Add more languages as needed */}
         </select>
       </div>
-      <input type="file" accept="image/*" onChange={handleImageUpload} />
-      {selectedImage && (
-        <img
-          src={URL.createObjectURL(selectedImage)}
-          alt="Selected"
-          style={{ maxWidth: "500px", marginTop: "20px" }}
-        />
-      )}
-      {translatedImage && (
-        <img
-          src={translatedImage}
-          alt="Translated"
-          style={{ maxWidth: "500px", marginTop: "20px" }}
-        />
-      )}
+      <input
+        type="file"
+        accept="image/*"
+        multiple
+        onChange={handleImageUpload}
+      />
+      <div style={{ display: "flex", flexWrap: "wrap", gap: "20px" }}>
+        {selectedImages.map((image, index) => (
+          <div
+            key={index}
+            style={{ textAlign: "center" }}
+            className="flex flex-row pl-8 pr-8 justify-around items-center w-full"
+          >
+            <img
+              src={URL.createObjectURL(image)}
+              alt={`Selected ${index}`}
+              style={{ maxWidth: "500px", marginTop: "20px" }}
+            />
+            {translatedImages[index] && (
+              <img
+                src={translatedImages[index]}
+                alt={`Translated ${index}`}
+                style={{ maxWidth: "500px", marginTop: "20px" }}
+              />
+            )}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
